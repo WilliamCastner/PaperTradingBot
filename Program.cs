512d28bf6109cd8bfb4c333b6eef4932c504bf9d
WilliamCastner <williambcastner@gmail.com>
2025-07-22 18:39:20 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Alpaca.Markets;
+using System.Threading.Tasks;
+
+namespace AlpacaExample
+{
+    internal static class Program
+    {
+        private const String KEY_ID = "PKIQY0NXPHL68U6QSUSQ";
+
+        private const String SECRET_KEY = "AXe5zO9RictRDNdg7BrvS0ZmuwAQ5I1Z2Z2Wvp3h";
+
+        public static async Task Main(string[] args)
+        {
+            // First, open the API connection
+            var client = Alpaca.Markets.Environments.Paper
+                .GetAlpacaTradingClient(new SecretKey(KEY_ID, SECRET_KEY));
+
+            // Get our account information.
+            var account = await client.GetAccountAsync();
+
+            // Check if our account is restricted from trading.
+            if (account.IsTradingBlocked)
+            {
+                Console.WriteLine("Account is currently restricted from trading.");
+            }
+
+            Console.WriteLine(account.BuyingPower + " is available as buying power.");
+
+            Console.Read();
+        }
+    }
+}
